ac235e65ea78729391ce5272b80c353e566f61f3
merna-hub <mernaelnahas65@gmail.com>
2026-02-17 15:59:12 +0200

    Add files via upload

---
+++ lib/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primary = Color(0xFFF5F5F5);
+  static const Color secondary = Color(0xFFE2A331);
+  static const Color textFieldColor = Color(0xFFE5E7EB);
+  static const Color contentOfField = Color(0xFF9CA3AF);
+  static const Color grayIcons = Color(0xFFB2B2B2);
+  static const Color offerColor = Color(0xFF8E8E93);
+  static const Color lastPrice = Color(0xFFAEAEB2);
+  static const Color quantity = Color(0xFF6B6B6B);
+}
